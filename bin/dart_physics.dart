e885cee3a0b10367534bdad3bbb40f665018d4ea
Landon <102832670+LostBitset@users.noreply.github.com>
2022-11-09 17:38:44 -0500

    lib code

--- bin/dart_physics.dart
+++ bin/dart_physics.dart
@@ -1,6 +1,6 @@
-import 'package:dart_physics/dart_physics.dart' as dart_physics;
+import 'package:dart_physics/dart_physics.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_physics.calculate()}!');
+	print('Got ${calculate()}.');
 }
 
--- lib/dart_physics.dart
+++ lib/dart_physics.dart
@@ -1,4 +1,36 @@
-int calculate() {
-  return 6 * 7;
+typedef Vec = List<num>;
+
+class ParticleState {
+	final Vec x;
+	final Vec v;
+
+	const ParticleState(this.x, this.v);
+}
+
+typedef Trajectory = Iterable<ParticleState>;
+
+abstract class Kinematic {
+	Vec get v_0;
+	Vec get a;
+
+	Trajectory traj(num timestep) sync* {
+		var v = List<num>.from(v_0);
+		var dim = v.length;
+		var x = List<num>.filled(dim, 0.0);
+		while (true) {
+			for (int i = 0; i < dim; i++) {
+				v[i] += a[i] * timestep;
+				x[i] += v[i] * timestep;
+			}
+			yield ParticleState(x, v);
+		}
+	}
+}
+
+class Dart extends Kinematic {
+	@override final Vec v_0;
+	@override final Vec a = [-9.8, 0.0, 0.0];
+
+	Dart(this.v_0);
 }
 
